828f2d790022dc64b89bef94c0694ae5af8b03ac
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2011-01-11 22:04:57 -0500

    Placed blank in about lazy seq

--- src/test/scala/org/functionalkoans/forscala/AboutLazySequences.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLazySequences.scala
@@ -9,30 +9,30 @@ class AboutLazySequences extends KoanSuite with ShouldMatchers {
   koan ("Creating a lazy collection form a strict collection") {
     val strictList = List(10, 20, 30)
     val lazyList = strictList.view
-    lazyList.head should be(strictList.head) 
+    lazyList.head should be(__) 
   }
   
   koan ("Strict collection always processes it elements but lazy collection does on demand") {
     var x = 0
     def inc = { x +=1; x }
     var strictList = List(inc _, inc _, inc _)
-    strictList.map(f => f()).head should be(1)
-    x should be(3)
+    strictList.map(f => f()).head should be(__)
+    x should be(__)
     strictList.map(f => f()).head    
-    x should be(6)
+    x should be(__)
     
     x = 0
     val lazyList = strictList.view
-    lazyList.map(f => f()).head should be(1)
-    x should be(1)
-    lazyList.map(f => f()).head should be(2)
-    x should be(2)    
+    lazyList.map(f => f()).head should be(__)
+    x should be(__)
+    lazyList.map(f => f()).head should be(__)
+    x should be(__)    
   }
   
   koan ("Lazy collection sometimes avoid processing errors") {
     val lazyList = List(2, -2, 0, 4).view map { 2 / _ }
-    lazyList.head should be(1)
-    lazyList(1) should be(-1)
+    lazyList.head should be(__)
+    lazyList(1) should be(__)
     intercept[ArithmeticException] {
       lazyList(2)
     }
@@ -40,8 +40,8 @@ class AboutLazySequences extends KoanSuite with ShouldMatchers {
   
   koan ("Lazy collections could also be infinite") {
     val infinite = Stream.from(1)
-    infinite.take(4).sum should be(10) 
-    Stream.continually(1).take(4).sum should be(4)
+    infinite.take(4).sum should be(__) 
+    Stream.continually(1).take(4).sum should be(__)
   }
   
   koan ("Always remember tail of a lazy collection is never computed unless required") {
@@ -49,8 +49,8 @@ class AboutLazySequences extends KoanSuite with ShouldMatchers {
       Stream.cons(value, makeLazy(value + 1))
     }
     val stream = makeLazy(1)
-    stream.head should be(1)
-    stream.tail.head should be(2)
+    stream.head should be(__)
+    stream.tail.head should be(__)
   }
   
 }
