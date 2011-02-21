841067aa039441b588c73f5c9aa53cd99958d405
dmarsh <dmarsh@srtsolutions.com>
2011-02-21 16:53:17 -0500

    added blanks

--- src/test/scala/org/functionalkoans/forscala/AboutRange.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutRange.scala
@@ -9,26 +9,26 @@ class AboutRange extends KoanSuite with ShouldMatchers {
    koan("Range are not inclusive at end of range") {
      val someNumbers = Range(0, 10)
     
-     someNumbers.size should be (10)
+     someNumbers.size should be (__)
    }
 
    koan("Range can specify increment") {
      val someNumbers = Range(2, 10, 3)
 
-     someNumbers.size should be (3)	
+     someNumbers.size should be (__)	
    }
 
    koan("Range can indicate inclusion") {
       val someNumbers = Range(0, 34, 2)
-      someNumbers.contains(33) should be (false)
-      someNumbers.contains(32) should be (true)
-      someNumbers.contains(34) should be (false)	
+      someNumbers.contains(33) should be (__)
+      someNumbers.contains(32) should be (__)
+      someNumbers.contains(34) should be (__)	
    }
 
    koan("Range can specify to include value") {
       val someNumbers = Range(0, 34).inclusive
 
-      someNumbers.contains(34) should be (true)	
+      someNumbers.contains(34) should be (__)	
    }
  
 }
