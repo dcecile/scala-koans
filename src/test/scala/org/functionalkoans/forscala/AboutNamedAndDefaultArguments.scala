d865596891c5ffb10c73ac2639eada619f59d460
dmarsh <dmarsh@srtsolutions.com>
2011-02-21 18:26:19 -0500

    added function parameter

--- src/test/scala/org/functionalkoans/forscala/AboutNamedAndDefaultArguments.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutNamedAndDefaultArguments.scala
@@ -75,4 +75,10 @@ class AboutNamedAndDefaultArguments() extends KoanSuite with ShouldMatchers {
 
     myColor should equal(__, __, __)
   }
+  koan("default parameters can be functional too") {
+      def reduce(a: Int, f: (Int, Int) => Int = (_ + _)): Int = f(a,a)
+
+      reduce(5) should equal (10)
+      reduce(5, _ * _) should equal (25)
+  }
 }
