0b4dc71eeb8a7a67fed6415336e9cd6040b5f017
Will Thames <will@musc.org.uk>
2011-05-02 19:16:48 +0100

    Added scalatest to project build information. Without this change I had very early test failures such as [error] /Users/will/src/scalakoansexercises/src/test/scala/org/functionalkoans/forscala/AboutAccessModifiers.scala:3: value scalatest is not a member of package org [error] import org.scalatest.matchers.ShouldMatchers

--- project/build/mavensbt.scala
+++ project/build/mavensbt.scala
@@ -2,4 +2,5 @@ import sbt._
 
 class FunctionalKoans(info: ProjectInfo) extends DefaultProject(info) {
   val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
+  val scalatest = "org.scalatest" % "scalatest" % "1.3-for-scala-2.8.1"
 }
