b09f2d71fcebc4d850edca6859182bfce7920e66
dmarsh <dmarsh@srtsolutions.com>
2011-05-10 14:23:49 -0400

    Split AboutConstructors into AboutClasses and AboutConstructors

---
+++ src/test/scala/org/functionalkoans/forscala/AboutClasses.scala
@@ -0,0 +1,43 @@
+package org.functionalkoans.forscala
+
+import org.scalatest.matchers.ShouldMatchers
+import support.BlankValues.__
+import support.KoanSuite
+
+class AboutClasses extends KoanSuite with ShouldMatchers {
+
+
+   // you can define class with var or val parameters
+   class ClassWithVarParameter(var description: String) {
+
+   }
+
+   koan("val parameters in class definition define getter") {
+	 val aClass = new ClassWithValParameter("name goes here")
+     aClass.name should be ("name goes here");
+   }
+
+   class ClassWithValParameter(val name: String) {
+
+   }
+
+   koan("var parameters in class definition define getter and setter") {
+     val aClass = new ClassWithVarParameter("description goes here")
+     aClass.description should be ("description goes here");
+
+     aClass.description = "new description"
+     aClass.description should be ("new description")
+   }
+
+   // you can define class with private fields
+   class ClassWithPrivateFields(name : String) {
+
+   }
+
+   koan("fields defined internally are private to class") {
+      val aClass = new ClassWithPrivateFields("name")
+
+      // NOTE: aClass.name is not accessible
+   }
+
+}
--- src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
@@ -6,47 +6,23 @@ import support.KoanSuite
 
 class AboutConstructors extends KoanSuite with ShouldMatchers {
 
-  class AboutConstructorWithValParameter(val name: String) {
-    // invoke auxilary constructor
+  class AboutConstructorWithAuxiliaryConstructor(val name: String) {
+    // invoke auxiliary constructor
     def this() {
       // what happens if you comment out the following line?
       this ("defaultname")
     }
   }
 
+
+  koan("Primary constructor specified with a parameter requires that parameter to be passed in") {
+    val aboutMe = new AboutConstructorWithAuxiliaryConstructor()
+
+  }
+
   class AboutClassWithNoClassParameter {
   }
 
-  class AboutConstructorWithVarParameter(var name: String) {
-  }
-
-  class AboutConstructorWithPrivateClassParameter(name: String) {
-  }
-
-  koan("val in class definition defines read only property") {
-    val aboutMe = new AboutConstructorWithValParameter("MyName")
-    aboutMe.name should be(__)
-
-
-  }
-
-  koan("var in class definition defines read/write parameters") {
-    val aboutMe = new AboutConstructorWithVarParameter("MyName")
-    aboutMe.name = "YourName"
-    aboutMe.name should be(__)
-  }
-
-  koan("private member data is not accessible") {
-    val aboutMe = new AboutConstructorWithPrivateClassParameter("MyName")
-
-    // what happens if you uncomment this line? why?
-    // aboutMe.name = "Me"
-  }
-
-  koan("Primary constructor specified with a parameter requires that parameter to be passed in") {
-    // val aboutMe = new AboutConstructorWithValParameter()
-
-  }
 
   koan("Class with no class parameters is called with no arguments") {
     // add parameter to make this fail
