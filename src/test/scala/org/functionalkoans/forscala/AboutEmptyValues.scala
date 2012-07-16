1770bcd8c73e77373bbd5f3f1d85b1f682ef33b4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2012-07-16 16:19:21 -0700

    Fixed Koan.

--- src/test/scala/org/functionalkoans/forscala/AboutEmptyValues.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutEmptyValues.scala
@@ -5,59 +5,59 @@ import support.KoanSuite
 
 class AboutEmptyValues extends KoanSuite {
 
-  test("None equals None") {
+  koan("None equals None") {
     assert(None === __)
   }
 
-  test("None should be identical to None") {
+  koan("None should be identical to None") {
     val a = None
     val b = None
     assert(a eq __)
   }
 
-  test("None can be converted to a String") {
+  koan("None can be converted to a String") {
     assert(None.toString === __)
   }
 
-  test("An empty list can be represented by another nothing value: Nil") {
+  koan("An empty list can be represented by another nothing value: Nil") {
     assert(List() === __)
   }
 
-  test("None can be converted to an empty list") {
+  koan("None can be converted to an empty list") {
     val a = None
     assert(a.toList === __)
   }
 
-  test("None is considered empty") {
+  koan("None is considered empty") {
     assert(None.isEmpty === __)
   }
 
-  test("None can be cast Any, AnyRef or AnyVal") {
+  koan("None can be cast Any, AnyRef or AnyVal") {
     assert(None.asInstanceOf[Any] === __)
     assert(None.asInstanceOf[AnyRef] === __)
     assert(None.asInstanceOf[AnyVal] === __)
   }
 
-  test("None cannot be cast to all types of objects") {
+  koan("None cannot be cast to all types of objects") {
     intercept[ClassCastException] {
       // put the exception you expect to see in place of the blank
       assert(None.asInstanceOf[String] === __)
     }
   }
 
-  test("None can be used with Option instead of null references") {
+  koan("None can be used with Option instead of null references") {
     val optional: Option[String] = None
     assert(optional.isEmpty === __)
     assert(optional === __)
   }
 
-  test("Some is the opposite of None for Option types") {
+  koan("Some is the opposite of None for Option types") {
     val optional: Option[String] = Some("Some Value")
     assert((optional == None) === __, "Some(value) should not equal None")
     assert(optional.isEmpty === __, "Some(value) should not be empty")
   }
 
-  test("Option.getOrElse can be used to provide a default in the case of None") {
+  koan("Option.getOrElse can be used to provide a default in the case of None") {
     val optional: Option[String] = Some("Some Value")
     val optional2: Option[String] = None
     assert(optional.getOrElse("No Value") === __, "Should return the value in the option")
