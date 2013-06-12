617ec97421d79313a8ba47feb9405ca5c4ce298f
danno <danno@Gazelle>
2013-06-11 21:49:33 -0600

    Changed message for better understanding

--- src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTuples.scala
@@ -25,4 +25,10 @@ class AboutTuples extends KoanSuite {
     tuple5._2 should be(__)
     tuple5._5 should be(__)
   }
+
+  koan("Tuples items can be swapped on a Tuple 2") {
+    val tuple = ("apple", 3).swap
+    tuple._1 should be(__)
+    tuple._2 should be(__)
+  }
 }
