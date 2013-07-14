5d6d3278d57ebf2b811d4fa378c13a2144de1cd6
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-07-14 17:37:51 -0600

    Added import to turn on implicit conversions

--- src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
@@ -1,18 +1,9 @@
 package org.functionalkoans.forscala
 
 import org.scalatest.matchers.ShouldMatchers
+import language.implicitConversions
 import support.KoanSuite
 
-
-/**
- * Created by Daniel Hinojosa
- * User: Daniel Hinojosa
- * Date: 3/10/11
- * Time: 5:38 PM
- * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
- * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
- * tel: 505.363.5832
- */
 class AboutImplicits extends KoanSuite with ShouldMatchers {
 
   koan("""Implicits wrap around existing classes to provide extra functionality
@@ -54,7 +45,7 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
     def add(a: BigInteger, b: BigInteger) = a.add(b)
 
-    (add(3, 6)) should be(__)
+    add(3, 6) should be(__)
   }
 
   koan("""Implicits can be used declare a value to be provided as a default as
@@ -64,6 +55,7 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
     def howMuchCanIMake_?(hours: Int)(implicit dollarsPerHour: BigDecimal) = dollarsPerHour * hours
 
     implicit var hourlyRate = BigDecimal(34.00)
+
     howMuchCanIMake_?(30) should be(__)
 
     hourlyRate = BigDecimal(95.00)
