ba0b12e131fe9a4aec0daa17d88e3afd4fc446cc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-07-14 17:44:40 -0600

    Added new means for using manifests

--- src/test/scala/org/functionalkoans/forscala/AboutManifests.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutManifests.scala
@@ -3,18 +3,7 @@ package org.functionalkoans.forscala
 import org.scalatest.matchers.ShouldMatchers
 import support.KoanSuite
 
-/**
- * Created by Daniel Hinojosa
- * User: Daniel Hinojosa
- * Date: 3/6/11
- * Time: 9:50 PM
- * url: <a href="http://www.evolutionnext.com">http://www.evolutionnext.com</a>
- * email: <a href="mailto:dhinojosa@evolutionnext.com">dhinojosa@evolutionnext.com</a>
- * tel: 505.363.5832
- */
-
 class Monkey
-
 
 class AboutManifests extends KoanSuite with ShouldMatchers {
   koan("""Manifests can be used to determine a type used
@@ -27,7 +16,7 @@ class AboutManifests extends KoanSuite with ShouldMatchers {
   koan("""Manifests can be attached to classes. Manifests have other meta-information about
           |  the type erased""") {
     class Barrel[T](implicit m: scala.reflect.Manifest[T]) {
-      def +(t: T) = "1 %s has been added".format(m.erasure.getSimpleName) //Simple-name of the class erased
+      def +(t: T) = "1 %s has been added".format(m.runtimeClass.getSimpleName) //Simple-name of the class erased
     }
     val monkeyBarrel = new Barrel[Monkey]
     (monkeyBarrel + new Monkey) should be(__)
