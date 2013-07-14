cf78559110d07f14cbadda3b0105cd5809b18b22
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-07-14 17:18:38 -0600

    Koan update to add new koans

--- src/test/scala/org/functionalkoans/forscala/Koans.scala
+++ src/test/scala/org/functionalkoans/forscala/Koans.scala
@@ -8,42 +8,52 @@ class Koans extends Suite {
     new AboutAsserts,
     new AboutValAndVar,
     new AboutClasses,
-    new AboutRange,
+    new AboutObjects,
+
     new AboutTuples,
     new AboutLists,
     new AboutMaps,
     new AboutSets,
+    new AboutOptions,
     new AboutMutableMaps,
     new AboutMutableSets,
     new AboutFormatting,
     new AboutPatternMatching,
     new AboutCaseClasses,
-    new AboutOptions,
-    new AboutSequencesAndArrays,
-    new AboutNamedAndDefaultArguments,
-    new AboutForExpressions,
-    new AboutEmptyValues,
-    new AboutParentClasses,
-    new AboutTypeSignatures,
-    new AboutTraits,
-    new AboutPreconditions,
+
+    new AboutRange,
     new AboutHigherOrderFunctions,
+    new AboutPartiallyAppliedFunctions,
+    new AboutPartialFunctions,
+
+    //Afternoon
+    new AboutImplicits,
+    new AboutTraits,
+    new AboutForExpressions,
+    new AboutInfixPrefixAndPostfixOperators,
+    new AboutInfixTypes,
+
+    new AboutSequencesAndArrays,
+    new AboutIterables,
+    new AboutTraversables,
+    new AboutNamedAndDefaultArguments,
+    new AboutManifests,
+    new AboutPreconditions,
+
+    //move this later type variance and signatures on slides and REPL
+
+    new AboutParentClasses,
+
+    new AboutEmptyValues,
+
+    new AboutTypeSignatures,
     new AboutUniformAccessPrinciple,
     new AboutLiteralBooleans,
     new AboutLiteralNumbers,
     new AboutLiteralStrings,
-    new AboutPartialFunctions,
-    new AboutPartiallyAppliedFunctions,
-    new AboutIterables,
-    new AboutTraversables,
-    new AboutInfixPrefixAndPostfixOperators,
-    new AboutInfixTypes,
-    new AboutImplicits,
-    new AboutManifests,
     new AboutTypeVariance,
     new AboutEnumerations,
     new AboutConstructors
-    
   )
 
   override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
