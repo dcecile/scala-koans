acbd7e15688f39a06f70b4d368b7413d647f2cd2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-11-12 23:11:18 -0700

    Fixed spacing in AboutInteroperability

--- src/test/scala/org/functionalkoans/forscala/AboutInteroperability.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutInteroperability.scala
@@ -3,7 +3,6 @@ package org.functionalkoans.forscala
 import support.KoanSuite
 import org.scalatest.matchers.ShouldMatchers
 
-
 class AboutInteroperability extends KoanSuite with ShouldMatchers {
   koan("""You can interop with a java class and it's use of collections by importing
           |   scala.collection.JavaConversions and letting scala implicitly convert
