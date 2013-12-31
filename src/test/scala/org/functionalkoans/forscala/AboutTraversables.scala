672ff667d4a1dcb050299d94b02c353da32a20f1
Steven Cook <cook_steven_j@cat.com>
2013-12-31 14:17:09 +1000

    Solution for koan was already filled in. Replaced solution with place holder markers.

--- src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTraversables.scala
@@ -302,8 +302,8 @@ class AboutTraversables extends KoanSuite with ShouldMatchers {
           |  is also defined as (xs takeWhile p, xs dropWhile p)""") {
     val array = Array(87, 44, 5, 4, 200, 10, 39, 100)
     val result = array span (_ < 100)
-    result._1 should be(Array(87, 44, 5, 4))
-    result._2 should be(Array(200, 10, 39, 100))
+    result._1 should be(Array(__, __, __, __))
+    result._2 should be(Array(__, __, __, __))
   }
 
   koan( """partition will split a Traversable according to predicate, return
